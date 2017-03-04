e58aea8a4b7282c61bf517eb2cf4663252028bf6
sherlynchua <sherlynchua.sc@gmail.com>
2017-03-04 20:11:02 +0800

    final

--- js/app1.js
+++ js/app1.js
@@ -1,36 +1,68 @@
-$(document).ready(function () {
-  })
+// **Switch make document ready first then run function
+$(document).ready(function() {
 
-var list = ['NYC', 'SF', 'LA', 'ATX', 'SYD']
+  //detect click update
+  $('#submit-btn').click(function(event){
+    event.preventDefault();
+    //take the vairable in text box make lowercase
+    var city = $('#city-type').val().toLowerCase();
+    //switch if city case is 'nyc'..to id'nyc' background
+    switch(city) {
+      case 'nyc':
+      case 'new york':
+      case 'new york city':
+        $('body').attr('id','nyc');
+        break;
+      case 'san francisco':
+      case 'bay area':
+      case 'sf':
+        $('body').attr('class', 'sf')
+        break;
+      case 'los angeles':
+      case 'la':
+      case 'lax' :
+        $('body').attr('class', 'la')
+        break;
+      case 'austin':
+      case 'atx':
+        $('body').attr('class', 'au')
+        break;
+      case 'sydney':
+      case 'sy':
+        $('body').attr('class', 'sy')
+        break;
+      default:
+    }
 
-list.forEach(function (item) {
-  var option = $('<option>').attr('value', item).text(item)
-  $('select').append(option)
 })
-
-
-
-$('select').change(function (){
-  var text = $(this).val()
-
-  if (text == 'NYC') {
-      $('body').attr('id', 'nyc')
-    }
-
-  else if (text == 'SF') {
-      $('body').attr('class', 'sf')
-    }
-
-  else if (text == 'LA') {
-      $('body').attr('class', 'la')
-    }
-
-  else if (text == 'ATX') {
-      $('body').attr('class', 'au')
-    }
-
-  else if (text == 'SYD') {
-      $('body').attr('class', 'sy')
-    }
-
 });
+
+// **IF ELSE make document ready first then run function
+// $(document).ready(function() {
+//
+//   //detect click update
+//   $('#submit-btn').click(function(event){
+//     event.preventDefault();
+//     //take the vairable in text box
+//     var city = $('#city-type').val().toLowerCase();
+//
+//     //if city is "new york" or "new york city" or "nyc" ** lowercase as alr change to lower on top.
+//     if (city === "nyc" || city === "new york"|| city === "new york city") {
+//     //nyc id already specify as background image...
+//     //so in body change the attr to nyc id..
+//       $('body').attr('id','nyc')
+//     }
+//     else if (city === "san francisco" || city === "bay area" || city === "sf") {
+//       $('body').attr('class', 'sf')
+//     }
+//     else if (city === "los angeles" || city === "la" || city === "lax") {
+//       $('body').attr('class', 'la')
+//     }
+//     else if (city === "austin" || city === "atx") {
+//       $('body').attr('class', 'au')
+//     }
+//     else if (city === "sydney" || city === "syd") {
+//                 $('body').attr('class', 'sy')
+//     }
+// })
+// });
